56a253ad9bcefa9f23223680426ca9d67b95a0a0
Michael Feingold <mfeingold@hill30.com>
2014-02-03 18:19:29 -0600

    a fix for 'clean session' + some extra logging

--- main/src/main/java/com/hill30/android/mqtt/paho/PahoConnection.java
+++ main/src/main/java/com/hill30/android/mqtt/paho/PahoConnection.java
@@ -1,8 +1,10 @@
 package com.hill30.android.mqtt.paho;
 
 import android.os.Looper;
+import android.util.Log;
 
 import com.hill30.android.mqtt.Connection;
+import com.hill30.android.mqtt.Service;
 
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -38,29 +40,43 @@ public abstract class PahoConnection extends Connection
         try {
             this.topic = topic;
             final String topic_name = topic + "." + LISTENER_TOPIC_SUFFIX + ".User";
+            if (mqttClient != null && mqttClient.isConnected())
+                mqttClient.disconnect(0, null, new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Log.d(TAG, "disconnected - success");
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.d(TAG, "disconnected - failure " + exception.getMessage());
+                    }
+                });
             mqttClient = new MqttAsyncClient(brokerUrl, clientID, new MqttDefaultFilePersistence(persistenceFolder));
             mqttClient.setCallback(new MqttCallback() {
                 @Override
                 public void connectionLost(Throwable cause) {
-
+                    Log.d(TAG, "connection lost cause: " + cause.getMessage());
                 }
 
                 @Override
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
                     onMessageRecieved(message.toString());
+                    Log.d(TAG, "message " + message + " received");
                 }
 
                 @Override
                 public void deliveryComplete(IMqttDeliveryToken token) {
-
+                    Log.d(TAG, "message delivery complete");
                 }
             });
             MqttConnectOptions options = new MqttConnectOptions();
-            options.setCleanSession(true);
+            options.setCleanSession(false);
             mqttClient.connect(options, null, new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
                     try {
+                        Log.d(TAG, "connection - success");
                         mqttClient.subscribe(topic_name, 2 /*QoS = EXACTLY_ONCE*/);
                     } catch (MqttException e) {
                         e.printStackTrace();
@@ -69,7 +85,7 @@ public abstract class PahoConnection extends Connection
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-
+                    Log.d(TAG, "connection failure: " + exception.getMessage());
                 }
             });
         } catch (MqttException e) {
@@ -79,10 +95,20 @@ public abstract class PahoConnection extends Connection
     }
 
     @Override
-    protected void send(String message) {
+    protected void send(final String message) {
         final String topic_name = topic + "." + SENDER_TOPIC_SUFFIX;
         try {
-            mqttClient.publish(topic_name, message.getBytes(), 2, true);
+            mqttClient.publish(topic_name, message.getBytes(), 2, true, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(TAG, "send message " + message + " - success");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d(TAG, "send message " + message + " - failure " + exception.getMessage());
+                }
+            });
         } catch (MqttException e) {
             e.printStackTrace();
         }
